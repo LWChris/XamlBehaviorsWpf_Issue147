c39d7ce9fceb354032388fbfa09559f87e4e978d
Chris <845152+LWChris@users.noreply.github.com>
2024-05-13 19:39:13 +0200

    Projektdateien hinzufügen.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace XamlBehaviorsTest
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            MakeReadOnlyCommand = new DelegateCommand<DependencyObject>(MakeReadOnly);
+        }
+
+        public ICommand MakeReadOnlyCommand { get; }
+
+        private void MakeReadOnly(DependencyObject parent)
+        {
+            SetTextBoxesReadOnly(parent, true);
+        }
+
+        private static void SetTextBoxesReadOnly(DependencyObject parent, bool isReadOnly)
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is TextBox textBox)
+                {
+                    textBox.IsReadOnly = isReadOnly;
+                } else
+                {
+                    SetTextBoxesReadOnly(child, isReadOnly);
+                }
+            }
+        }
+    }
+}
